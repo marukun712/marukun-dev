92a62375e80c939396fc0587a26859ef75e57f31
marukun712 <marukun530@gmail.com>
2024-02-13 08:01:19 +0000

    ⚡️レスポンシブ対応2

--- components/blog/Search.tsx
+++ components/blog/Search.tsx
@@ -12,7 +12,7 @@ export default function Search() {
     }
 
     return (
-        <div className="md:px-10">
+        <div className="px-10">
             <h1 className="text-2xl">サイト内検索</h1>
             <Input value={text} placeholder="キーワードを入力..." onChange={(e) => setText(e.target.value)} />
             <Button onClick={() => { handleSubmit(); }}>検索</Button>
